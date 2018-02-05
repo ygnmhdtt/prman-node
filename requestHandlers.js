a570f1e64e17dd2379d662384795fbefeda3e5dd
ygnmhdtt <yaginuma@mmmcorp.co.jp>
2018-02-05 21:39:03 +0900

    fix prmanStart

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,8 +1,19 @@
-function prmanStart(response) {
+function prmanStart(request, response) {
+  let slackRequest = require("./slack")
+  let req = new slackRequest(request.body);
+  req.init();
+
+  // ToDo: Token Validation
+
+  const user = req.getUser();
+
+  // ToDo: getGithubUser
+  // ToDo: getRepos
+  // ToDo: getPRs
+
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.write("Hello-world");
   response.end();
 }
-
 
 exports.prmanStart = prmanStart;
